541ef744f8a68ca219e6322624570947a79b55ce
justanothernormalusername <marcusdeng2019@gmail.com>
2026-07-24 21:55:22 -0700

    First commit

---
+++ src/main/java/com/serenity/SerenityReforgedClient.java
@@ -0,0 +1,10 @@
+package com.serenity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SerenityReforgedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/serenity/SerenityReforgedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.serenity;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SerenityReforgedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
